66bf2c6e580dc5857aeec4b965a3a5d6fe96c19c
Fatemeh Kiaie <40069260+fatemehKi@users.noreply.github.com>
2019-07-09 00:25:04 -0400

    Update project_LR.py

--- project_LR.py
+++ project_LR.py
@@ -40,13 +40,27 @@ def cor(df):
     cor = df.corr()
     sns.heatmap(cor,annot=True)
 
-##### Normalization
-def splt(df):
-    X=df[]
-    X_train = df.sample(frac=0.8,random_state=0)
-    X_test = df.drop(X_train.index)
+##### Splitting--assuming the last column is the target
+def splt(df, fr):
+    df_train = df.sample(frac=fr,random_state=0)
+    X_train=df_train.iloc[:,:-1]
+    y_train=dataset.iloc[:,-1]
+    df_test = df.drop(df_train.index)
+    X_test=df_test.iloc[:,:-1]
+    y_test=df_test.iloc[:,-1]
+    return(X_train, y_train, X_test, y_test)
 
+##### Initilization
+def init:
+    X = tf.constant(np.random.randn(3,1), name = "X")
+    W = tf.constant(np.random.randn(4,3), name = "W")
+    b = tf.constant(np.random.randn(4,1), name = "b")
+    Y = tf.add(tf.matmul(W, X),b)
 
+#### Model
+def LR()
+    x=tf.placeholder(tf)
+    
    
 if __name__ == '__main__':  
     
@@ -56,5 +70,52 @@ if __name__ == '__main__':
     dataset=cleaning(dataset)
     dataset=rmv_usls(dataset, 'url')
     cor(dataset)
+    fr=0.8 #fraction of splitting
+    X_train, y_train, X_test, y_test= splt(dataset,fr)
+    eta =tf.constant(0.01, name='learning_rate')
+    stp = tf.constant(1000, name='no_step')
+    dis_stp = tf.constant(100, name='display_step')
+    W = tf.placeholder(np.random.randn())
+    b = tf.placeholder(np.random.randn())
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+# Linear regression (Wx + b)
+def linear_regression(inputs):
+    return inputs * W + b
+
+
+# Mean square error
+def mean_square_fn(model_fn, inputs, labels):
+    return tf.reduce_sum(tf.pow(model_fn(inputs) - labels, 2)) / (2 * n_samples)
+
+
+# SGD Optimizer
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+# Compute gradients
+grad = tfe.implicit_gradients(mean_square_fn)
+
+# Initial cost, before optimizing
+print("Initial cost= {:.9f}".format(
+    mean_square_fn(linear_regression, train_X, train_Y)),
+    "W=", W.numpy(), "b=", b.numpy())
+
+# Training
+for step in range(num_steps):
+
+    optimizer.apply_gradients(grad(linear_regression, train_X, train_Y))
+
+    if (step + 1) % display_step == 0 or step == 0:
+        print("Epoch:", '%04d' % (step + 1), "cost=",
+              "{:.9f}".format(mean_square_fn(linear_regression, train_X, train_Y)),
+              "W=", W.numpy(), "b=", b.numpy())
     
